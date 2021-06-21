0b01eaa3e672650beb7e59503c4e76ded14a85db
dd90001 <1292644504@qq.com>
2021-06-21 11:19:34 +0800

    14 commit

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,18 +1,18 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL = 'https://api.vdswap.com/token_lists/index.php'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  't2crtokens.eth', // kleros
-  'tokens.1inch.eth', // 1inch
-  'synths.snx.eth',
-  'tokenlist.dharma.eth',
-  'defi.cmc.eth',
-  'erc20.cmc.eth',
-  'stablecoin.cmc.eth',
-  'tokenlist.zerion.eth',
-  'tokenlist.aave.eth',
-  'https://api.vdswap.com/token_lists/index.php'
+//   't2crtokens.eth', // kleros
+//   'tokens.1inch.eth', // 1inch
+//   'synths.snx.eth',
+//   'tokenlist.dharma.eth',
+//   'defi.cmc.eth',
+//   'erc20.cmc.eth',
+//   'stablecoin.cmc.eth',
+//   'tokenlist.zerion.eth',
+//   'tokenlist.aave.eth',
+//   'https://api.vdswap.com/token_lists/index.php'
   //   'https://tokens.coingecko.com/uniswap/all.json',
   //   'https://app.tryroll.com/tokens.json',
   //   'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
