d1ec95c1d39708ba8157a64d721d59a32d223cce
dd90001 <1292644504@qq.com>
2021-06-21 11:48:43 +0800

    14 commit

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://api.vdswap.com/token_lists/index.php'
+export const DEFAULT_TOKEN_LIST_URL = window.location.origin + '/tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
